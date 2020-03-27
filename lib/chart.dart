79163d4ccd56fd03a6ab3c40f2fda46a2cb35515
austinabell <austinabell8@gmail.com>
2020-03-27 15:23:23 -0400

    Fuck your pie chart library

--- lib/chart.dart
+++ lib/chart.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
+// import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:provider/provider.dart';
 import 'package:myapp/schedule.dart';
 
@@ -7,9 +7,18 @@ class SimplePieChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<MySchedule>(
-      builder: (context, state, _) => charts.PieChart(
-        state.seriesPieData,
-      ),
-    );
+        builder: (context, state, _) =>
+            Center(child: Text("${state.stateManagementTime}")));
   }
 }
+
+// class SimplePieChart extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Consumer<MySchedule>(
+//       builder: (context, state, _) => charts.PieChart(
+//         state.seriesPieData,
+//       ),
+//     );
+//   }
+// }
--- lib/schedule.dart
+++ lib/schedule.dart
@@ -1,42 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
 
 class MySchedule with ChangeNotifier {
-  Task modifiableTask = new Task("Work", 35.4, Color(0xff123423));
-  double get stateManagementTime => modifiableTask.taskValue;
+  double _stateManagementTime = 40;
+  double get stateManagementTime => _stateManagementTime;
 
   set stateManagementTime(double newValue) {
-    modifiableTask.taskValue = newValue;
-    // lol this is a hack but idc I'm lazy
-    seriesPieData = [seriesPieData[0]];
+    _stateManagementTime = newValue;
     notifyListeners();
   }
-
-  List<charts.Series<Task, String>> seriesPieData;
-
-  MySchedule() {
-    this.seriesPieData = [
-      charts.Series(
-        domainFn: (Task task, _) => task.task,
-        measureFn: (Task task, _) => task.taskValue,
-        colorFn: (Task task, _) =>
-            charts.ColorUtil.fromDartColor(task.colorVal),
-        id: "Daily Task",
-        data: [
-          modifiableTask,
-          new Task("Play", 5, Color(0xff575757)),
-          new Task("Eat", 31, Color(0xff203954)),
-          new Task("Sleep", 14, Color(0xff223456)),
-        ],
-        labelAccessorFn: (Task row, _) => '${row.taskValue}',
-      ),
-    ];
-  }
-}
-
-class Task {
-  String task;
-  double taskValue;
-  Color colorVal;
-  Task(this.task, this.taskValue, this.colorVal);
 }
